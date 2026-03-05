bc671d9870b3d58ccd6d86e96fb171b6ee51c791
Hina-Wajid <U2504335@rts-365.dk>
2026-03-05 08:20:48 +0100

    async-branch

--- details.js
+++ details.js
@@ -6,14 +6,40 @@ const wrapperDetail = document.querySelector(".wrapper-detail");
 const headerPokemonDetail = document.querySelector("#header__pokemon-detail");
 const mainPokemonDetail = document.querySelector("#main__pokemon-detail");
 
+let species = "hej";
 
-
+/* async function getData() {
+  const [users, posts] = await Promise.all([
+    fetch("/users").then(r => r.json()),
+    fetch("/posts").then(r => r.json())
+  ]);
+ 
+  console.log(users, posts);
+} */
 const url = `https://pokeapi.co/api/v2/pokemon/${id}/`;
 fetch(url)
     .then((respons) => respons.json())
     .then((data) => {
+        fetch(data.species.url)
+            .then((respons) => respons.json())
+            .then((data) => {
+                console.log(data);
+
+                species = data.flavor_text_entries.reduce((accumulator, currentValue) => {
+                    console.log(currentValue.language.name);
+
+
+                    if (currentValue.language.name == 'en') {
+                        return accumulator + currentValue.flavor_text;
+                    }
+                }, "")
+            })
         showPokemonCard(data);
     })
+
+
+
+
 
 function showPokemonCard(pokemonInfoData) {
     const { weight } = pokemonInfoData;
@@ -21,12 +47,13 @@ function showPokemonCard(pokemonInfoData) {
     const urlimage = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`
     const backgroundColorClass = `background-${pokemonInfoData.types[0].type.name}`;
     wrapperDetail.classList.add(backgroundColorClass);
+    const text_color = `text_color-${pokemonInfoData.types[0].type.name}`
     //header 
-    const headerString = `
+    const headerString = /* html */`
       <div class = "div__header_styling">
             <a class="back2-button" href="index.html"><img src="assets/back2-arrow.svg" alt=""></a>
-            <h2>${pokemonInfoData.forms[0].name}</h2>
-            <p>#${entryNumber}</p>
+            <h2 class="text_color--white"> ${pokemonInfoData.forms[0].name}</h2>
+            <p class="text_color--white">#${entryNumber}</p>
     </div>
 `
     headerPokemonDetail.insertAdjacentHTML("afterbegin", headerString);
@@ -43,38 +70,38 @@ function showPokemonCard(pokemonInfoData) {
             </div>
         </figure>          
 `
-
     mainPokemonDetail.insertAdjacentHTML("beforeend", mainString);
 
     // mainPokemonDetail.insertAdjacentHTML("beforeend", aboutString);
-    const listItemsString = /*html*/`<ul> ${pokemonInfoData.types.map((thislink) => `<li>${thislink.type.name}</li>`).join("")}</ul>`
+    const listItemsString = /*html*/`<ul> ${pokemonInfoData.types.map((thistype) => `<li>${thistype.type.name}</li>`).join("")}</ul>`
     console.log(listItemsString);
     console.log(pokemonInfoData.types);
     pokemonInfoData.types.map((element) => {
         `<li>${element.type.name}</li>`
     })
-
+    /*  <div class ="pil_wrapper">
+               <p class ="pil background-${pokemonInfoData.types[0].type.name}">${pokemonInfoData.types[0].type.name}</p>
+               <p class ="pil background-${pokemonInfoData.types[1].type.name}">${pokemonInfoData.types[1].type.name}</p>
+            </div> */
     const aboutInfoString = /*html*/`
 
  <article class ="about_article"> 
         <div class ="pil_wrapper">
-           <p class ="pil background-${pokemonInfoData.types[0].type.name}">${pokemonInfoData.types[0].type.name}</p>
-           <p class ="pil background-${pokemonInfoData.types[1].type.name}">${pokemonInfoData.types[1].type.name}</p>
-  
-
+            ${pokemonInfoData.types.map((thistype) => `<p class ="pil background-${thistype.type.name}"> ${thistype.type.name}</p>`).join("")}
         </div>
-            <h2>About </h2>
-            <section class ="section__about">
-                <div>
-              <figure class ="flex-display">
+        <section class ="section__about margin__top">  
+            <h2 class = "${text_color}">About </h2>
+            <section class ="flex-display section__inside_about">
+                <div class ="card_inside">
+                  <figure class ="flex-display">
                     <img src="assets/weight.svg" alt="Weight">
                     <figcaption>
                         <p>${pokemonInfoData.weight / 10} kg</p>
                     </figcaption>
-                    </figure>
+                   </figure>
                     <h3>Weight</h3>
                 </div>
-                 <div >
+                 <div class ="card_inside">
                  <figure class ="flex-display">
                     <img src="assets/straighten.svg" alt="straighten">
                     <figcaption>
@@ -82,9 +109,8 @@ function showPokemonCard(pokemonInfoData) {
                     </figcaption>
                     </figure>
                     <h3>Height</h3>
-
         </div>
-         <div>
+         <div class ="card_inside">
         <ul class ="ul__abilities">
         ${pokemonInfoData.abilities.map((item) => {
         return `<li>${item.ability.name}</li>`
@@ -92,18 +118,14 @@ function showPokemonCard(pokemonInfoData) {
         <h3>Moves</h3>
         </div>
     </section>
+    </section>
+    <p class ="margin__top">${species}</p>
     
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quam, quia ex nostrum esse iure illum quisquam
-                incidunt eaque sed architecto similique voluptatum ratione consequuntur deleniti minus perspiciatis sint
-                aspernatur beatae!</p>
-    
-                 
-    <table class="table__stats">${pokemonInfoData.stats.map(pokeStats => `<tr><th>${pokeStats.stat.name}</th><td><label for="">${pokeStats.base_stat}</label></td><td><progress value="${pokeStats.base_stat}" max="230"></progress></td></tr>`).join(" ")}</table> 
- </article>
-
-
-
-
+     <section class ="section__stats margin__top"> 
+     <h2 class = "${text_color}">Base Stats</h2>             
+        <table class="table__stats">${pokemonInfoData.stats.map(pokeStats => `<tr><th class ="${text_color}">${pokeStats.stat.name}</th><td><label for="">${pokeStats.base_stat}</label></td><td><progress value="${pokeStats.base_stat}" max="230"></progress></td></tr>`).join(" ")}</table> 
+      </section>    
+    </article>
      `
 
     mainPokemonDetail.insertAdjacentHTML("beforeend", aboutInfoString);
@@ -113,13 +135,6 @@ function showPokemonCard(pokemonInfoData) {
 
 
     console.log(pokemonInfoData.types[0].type.name);
-    /* const typeNameString = pokemonInfoData.types.map(element => {
-        return element.type.name;
-    }); */
-
-    //pokemonInfoData.types.forEach(element => { <p class="pil"> ${element.type.name}</p> });
-
-
 
     // here progress
 
@@ -129,9 +144,3 @@ function showPokemonCard(pokemonInfoData) {
 }
 /* class ="background-${pokemonInfoData.types[0].type.name}" */
 
-/* <figure class="gallery">
-            <button class="gallery__right prev">&lt;</button>
-            <img src="assets/Silhouette.png" alt="">
-            <button class="gallery__left next">&gt;</button>
-        </figure> 
-        */
